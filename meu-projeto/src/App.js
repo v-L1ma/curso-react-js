3bd08dc4c2f2f8d3cb97bd83df4ef1c3114de242
Vinicius Lima <Viniciuslima957@hotmail.com>
2024-11-28 23:09:30 -0300

    renderizando listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 
 function App() {  
+  const meusItems = ['React', 'Vue', 'Angular']
 
   return (
     <div className="App">
-      <h1>Testando Condicionais</h1>
-      <Condicional />
+      <h1>Renderizando listas</h1>
+      <OutraLista itens={meusItems} />
     </div>
   );
 }
