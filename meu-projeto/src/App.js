e4e47f45419c47759d62b9f16b6a9ad54fd7f0a2
Vinicius Lima <Viniciuslima957@hotmail.com>
2024-12-01 13:40:01 -0300

    finalizando a parte teorica

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,16 +1,34 @@
-import './App.css';
-import Condicional from './components/Condicional';
-import OutraLista from './components/OutraLista';
-
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import Home from './pages/Home'
+import { ImAirplane } from "react-icons/im";
 
 function App() {  
   const meusItems = ['React', 'Vue', 'Angular']
 
   return (
-    <div className="App">
-      <h1>Renderizando listas</h1>
-      <OutraLista itens={meusItems} />
-    </div>
+    <Router>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Empresa">Empresa</Link>
+      </li>
+      <li>
+        <Link to="/Contato">Contato</Link>
+      </li>
+      <ImAirplane />
+    </ul>
+
+    <Routes>
+      <Route exact path="/" element={<Home />}  />
+      <Route path='/Empresa' element={<Empresa />} />
+      <Route path='/Contato' element={<Contato />} />
+    </Routes>
+
+    </Router>
   );
 }
 
